c8eb412061b01f7cf67e479373ff67b58dcacccd
ajiejot <nolasco.ajie@gmail.com>
2018-07-30 21:20:00 +0800

    [fixed]: layout and added some state and events

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,68 @@
 import React from 'react'
 const FA = require('react-fontawesome');
 import './styles.css';
+import Logo from './logo.png'
 
 export default class App extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state={
+            isError: false,
+            errorMsg: '',
+            number: '',
+            message: '',
+        }
+    }
+
+    onChangeHandle(e){
+        console.log(e.target.name)
+    }
+    
+    onSubmitHandle(e){
+        this.setState({
+            isError: !this.state.isError
+        })
+    }
+
     render(){
         return(
         <div className="container">
-            <div className="row">
-                <div className="col-md-5 offset-md-3">
-                    <div className="card-body">
+            <div className="row" >
+                <div className="col-md-4 offset-md-4">
+                    <div className="card-body" >
                         <div className="card row">
                             <div className="card-header">
-
                                 &nbsp;
                             </div>
                             <form className="form-group no-padding">
-                                <div className="input-group mb-1">
-                                        <div className="input-group-prepend "> 
-                                            <label className="input-group-text rounded-0" htmlFor="name">
-                                            <FA style={{'width':'15px'}} name="user"/></label>
-                                        </div>
-                                            <input type="text" className="form-control rounded-0 form-control-sm" 
-                                            id="name" name="name" placeholder="Name"/>
+                            <div style={{'marginTop':'-4px'}} className="input-group mb-1">
+                                {this.state.isError ? 
+                                    <div className="status">
+                                        <label>Message Sent!!</label>
+                                    </div>
+                                    :''
+                                    }
                                 </div>
                                 <div style={{'marginTop':'-4px'}} className="input-group mb-1">
                                        <div className="input-group-prepend "> 
                                             <label className="input-group-text rounded-0" htmlFor="number">
                                             <FA style={{'width':'15px'}} name="phone-square"/></label>
                                         </div>
-                                            <input type="text" className="form-control rounded-0 form-control-sm" 
+                                            <input onChange={this.onChangeHandle.bind(this)}
+                                             type="text" className="form-control rounded-0 error" 
                                             id="number" name="number" placeholder="09123456789"/>   
                                 </div>
                                 <div style={{'marginTop':'-4px'}} className="input-group mb-1">
                                        <div className="input-group-prepend "> 
-                                            <label className="input-group-text rounded-0" htmlFor="number">
-                                            <FA style={{'width':'15px'}} name="comment"/></label>
+                                            <label className="input-group-text rounded-0">
+                                            <FA  style={{'width':'15px'}} name="comment"/></label>
                                         </div>
-                                        <textarea placeholder="......" style={{'resize':'none','height':'150px'}} className="form-control rounded-0" aria-label="With textarea"></textarea> 
+                                        <textarea onChange={this.onChangeHandle.bind(this)} name="message" placeholder="......" style={{'resize':'none','height':'200px'}}
+                                         className="form-control rounded-0 success"></textarea> 
                                 </div>
-                                <div style={{'marginTop':'-4px','textAlign':'center'}}>
-                                <button type="button" className="btn btn-success btn-lg btn-block">SEND</button>
+                                <div style={{'marginTop':'-4px','textAlign':'center','marginBottom':'-16px'}}>
+                                <button onClick={this.onSubmitHandle.bind(this)} type="button" className="btn btn-success btn-lg btn-block ">SEND</button>
                                 </div>
                             </form>
                         </div>
