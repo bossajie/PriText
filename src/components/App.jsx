17e20996e3879af5c6c52b3aa0bf49ca00dc7fad
ajiejot <nolasco.ajie@gmail.com>
2019-09-20 21:56:19 +0800

    -fixed cors error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,15 +24,17 @@ export default class App extends React.Component{
             maxLengthMessage: '',
         }
     }
+    componentDidMount(){
+
+    }
 
     //every onchange event on the number and textmessage inputs.
     onChangeHandle(e){
         //For input number validation
         if(e.target.name==='number'){
             const re = /^[0-9\b]+$/;
-            console.log(this.state.number.length)
             if (e.target.value=='' || re.test(e.target.value)){
-                this.setState({number:e.target.value})
+                this.setState({number:e.target.value,isError:false})
             }
         }
         else{
@@ -44,10 +46,12 @@ export default class App extends React.Component{
             })
         }
 
-        //check for number  prefix if invalid
-        if (e.target.value.length>4 && e.target.name==='number'){
+    }
+    //check for number  prefix if invalid
+    onBlurHandle(e){
+        if (e.target.name==='number'){
             let substring=e.target.value.substring(1,4)
-            if(prefix.indexOf(parseInt(substring)) > -1 && e.target.value.length===11 ){
+            if(prefix.indexOf(parseInt(substring)) > -1 && e.target.value.length===11 || e.target.value===''){
                 this.setState({
                     errorMsg: '',
                     isError: false,
@@ -60,36 +64,60 @@ export default class App extends React.Component{
                 })
             }
         }
-
     }
 
 
     // SENT THE MESSAGE NOW!
     onSubmitHandle(e){
-        const payload ={
-            "1": this.state.number,
-            "2": this.state.message,
-            "3": APIKEY,
+
+
+        // check number, it should be exact 11 digits
+        if (this.state.number.length!==11){
+            this.setState({
+                isError:true,
+                errorMsg: 'Invalid. Phone number should be 11 digits.'
+            })
         }
-        request
-        .post('https://www.itexmo.com/php_api/api.php')
-        .set('Content-Type', 'application/x-www-form-urlencoded')
-        .send(payload)
-        .end(function(err, res){
-            if (res.text===0){
-                this.setState({
-                    isError: true,
-                    errorMsg: 'Message Sent!',
-                    number:'',
-                    message:'',
-                    maxLengthMessage:'',
-                })
-                console.log('message sent')
+        // message should not more than 100 characters and not empty
+        if (this.state.message.length===0 && this.state.message.length<100){
+            this.setState({
+                isError: true,
+                errorMsg: 'Invalid. Message should not be empty and not more than 100 characters.'
+            })
+        }
+        else{
+            const payload ={
+                "1": this.state.number,
+                "2": this.state.message,
+                "3": APIKEY,
             }
-            else{
-                alert('message failed')
-            }
-        }); 
+            request
+            .post('https://cors-anywhere.herokuapp.com/https://www.itexmo.com/php_api/api.php')
+            .set('Content-Type', 'application/x-www-form-urlencoded')
+            .set('Access-Control-Allow-Origin','*')
+            .set('accept', 'json')
+            .send(payload)
+            .end((err, res) => {
+                // "0" = Sent , "3" = Failed
+                if (res.text==="0"){
+                    this.setState({
+                        isError: true,
+                        errorMsg: 'Message Sent!',
+                        number:'',
+                        message:'',
+                        maxLengthMessage:'',
+                    })
+                    console.log('message sent')
+                }
+                else{
+                    this.setState({
+                        isError: true,
+                        errorMsg: 'Message Failed!',
+                    })
+                    alert('message failed')
+                }
+            }); 
+        }
 
 
     }
@@ -98,83 +126,84 @@ export default class App extends React.Component{
         console.log('Recaptcha loaded')
     }
     executeCaptcha(e){
-
         e.preventDefault()
         this.state.recaptchaInstance.execute();
     }
 
     render(){
-        return(
-        <div className="container">
-            <div className="row" >
-                <div className="col-md-4 offset-md-4">
-                    <div className="card-body" >
-                        <div className="card row">
-                            <div className="card-header">
-                                &nbsp;
-                            </div>
-                            <form className="form-group no-padding"  onSubmit={this.executeCaptcha.bind(this)} >
-                            <div style={{'marginTop':'-4px'}} className="input-group mb-1">
-                                {this.state.isError ? 
-                                    <div className="status">
-                                        <label style={this.state.errorMsg==='Invalid Number' ? {'color':'red',}: {}}>{this.state.errorMsg}</label>
+
+            return(
+            <div className="container">
+                <div className="row" >
+                    <div className="col-md-4 offset-md-4">
+                        <div className="card-body" >
+                            <div className="card row">
+                                <div className="card-header">
+                                    &nbsp;
+                                </div>
+                                <form className="form-group no-padding"  onSubmit={this.executeCaptcha.bind(this)} >
+                                <div style={{'marginTop':'-4px'}} className="input-group mb-1">
+                                    {this.state.isError ? 
+                                        <div className="status">
+                                            <label style={this.state.errorMsg.includes('Invalid') || this.state.errorMsg.includes('Failed') ? {'color':'red',}: {}}>{this.state.errorMsg}</label>
+                                        </div>
+                                        :''
+                                        }
                                     </div>
-                                    :''
-                                    }
-                                </div>
-                                <div style={{'marginTop':'-4px'}} className="input-group mb-1">
-                                       <div className="input-group-prepend "> 
-                                            <label className="input-group-text rounded-0" htmlFor="number">
-                                            <FA style={{'width':'15px'}} name="phone-square"/></label>
-                                        </div>
-                                            <input
-                                                pattern=".{11}"
+                                    <div style={{'marginTop':'-4px'}} className="input-group mb-1">
+                                        <div className="input-group-prepend "> 
+                                                <label className="input-group-text rounded-0" htmlFor="number">
+                                                <FA style={{'width':'15px'}} name="phone-square"/></label>
+                                            </div>
+                                                <input
+                                                    onBlur={this.onBlurHandle.bind(this)}
+                                                    pattern=".{11}"
+                                                    required
+                                                    maxLength="11" 
+                                                    onChange={this.onChangeHandle.bind(this)}
+                                                    value={this.state.number}
+                                                    type="text" 
+                                                    className={this.state.number.length!==11 ? "form-control rounded-0 error" : "form-control rounded-0 success" }
+                                                    id="number" name="number" placeholder="11 DIGITS NUMBER (09125990393)"
+                                                />   
+                                    </div>
+                                    <div style={{'marginTop':'-4px'}} className="input-group mb-1">
+                                        <div className="input-group-prepend "> 
+                                                <label className="input-group-text rounded-0">
+                                                <FA  style={{'width':'15px'}} name="comment"/></label>
+                                            </div>
+                                            <textarea
+                                                pattern=".{100}"
+                                                maxLength="100" 
                                                 required
-                                                maxLength="11" 
                                                 onChange={this.onChangeHandle.bind(this)}
-                                                value={this.state.number}
-                                                type="text" 
-                                                className={this.state.number.length!==11 ? "form-control rounded-0 error" : "form-control rounded-0 success" }
-                                                id="number" name="number" placeholder="11 DIGITS NUMBER"
-                                             />   
-                                </div>
-                                <div style={{'marginTop':'-4px'}} className="input-group mb-1">
-                                       <div className="input-group-prepend "> 
-                                            <label className="input-group-text rounded-0">
-                                            <FA  style={{'width':'15px'}} name="comment"/></label>
-                                        </div>
-                                        <textarea
-                                            pattern=".{100}"
-                                            maxLength="100" 
-                                            required
-                                            onChange={this.onChangeHandle.bind(this)}
-                                            value={this.state.message} 
-                                            name="message" placeholder="......" 
-                                            style={{'resize':'none','height':'200px'}}
-                                            className={this.state.message.length===0? "form-control rounded-0 error" : "form-control rounded-0 success" }
-                                            >
-                                         </textarea> 
-                                </div>
-                                <div style={{'textAlign':'center','fontSize':'11px'}} >
-                                    <label>{this.state.maxLengthMessage}</label>
-                                </div>
-                                <div style={{'marginTop':'-4px','textAlign':'center','marginBottom':'-16px'}}>
-                                <button disabled={this.state.isError} className="btn btn-success btn-lg btn-block">SEND</button>
-                                <Recaptcha
-                                ref={e => this.state.recaptchaInstance = e}
-                                render="explicit"
-                                sitekey="6LfOV2cUAAAAAAu7BQKDdDzppHxi8ZCIseVqIyKK"
-                                size="invisible"
-                                verifyCallback={this.onSubmitHandle.bind(this)}
-                                onloadCallback= {this.reCaptchaOnload.bind(this)}
-                                />
-                                </div>
-                            </form>
+                                                value={this.state.message} 
+                                                name="message" placeholder="......" 
+                                                style={{'resize':'none','height':'200px'}}
+                                                className={this.state.message.length===0? "form-control rounded-0 error" : "form-control rounded-0 success" }
+                                                >
+                                            </textarea> 
+                                    </div>
+                                    <div style={{'textAlign':'center','fontSize':'11px'}} >
+                                        <label>{this.state.maxLengthMessage}</label>
+                                    </div>
+                                    <div style={{'marginTop':'-4px','textAlign':'center','marginBottom':'-16px'}}>
+                                    <button disabled={this.state.isError} className="btn btn-success btn-lg btn-block">SEND</button>
+                                    <Recaptcha
+                                        ref={e => this.state.recaptchaInstance = e}
+                                        render="explicit"
+                                        sitekey="6LfOV2cUAAAAAAu7BQKDdDzppHxi8ZCIseVqIyKK"
+                                        size="invisible"
+                                        verifyCallback={this.onSubmitHandle.bind(this)}
+                                        onloadCallback= {this.reCaptchaOnload.bind(this)}
+                                    />
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
             )
     }
 }
